a035838838afdbbbecc6b07809f8ad3e724e33f0
mkmalakzai <mkmalakzai@gmail.com>
2026-09-23 19:04:43 +0430

    Fix multi-admin access across ADMIN panel
    * Allow configured admins: admin_balance_add_start.js
    
    * Allow configured admins: admin_balance_amount.js
    
    * Allow configured admins: admin_balance_confirm.js
    
    * Allow configured admins: admin_balance_deduct_start.js
    
    * Allow configured admins: admin_balance_reason.js
    
    * Allow configured admins: admin_broadcast.js
    
    * Allow configured admins: admin_broadcast_receive.js
    
    * Allow configured admins: admin_broadcast_send.js
    
    * Allow configured admins: admin_currency.js
    
    * Allow configured admins: admin_daily_bonus.js
    
    * Allow configured admins: admin_proof_approve.js
    
    * Allow configured admins: admin_proof_reject.js
    
    * Allow configured admins: admin_proof_view.js
    
    * Allow configured admins: admin_proofs.js
    
    * Allow configured admins: admin_referral.js
    
    * Fix admin access: admin_statistics.js
    
    * Fix admin access: admin_task_delete.js
    
    * Fix admin access: admin_task_delete_confirm.js
    
    * Fix admin access: admin_task_edit.js
    
    * Fix admin access: admin_task_list.js
    
    * Allow configured admins: admin_task_toggle.js
    
    * Allow configured admins: admin_task_view.js
    
    * Allow configured admins: admin_tasks.js
    
    * Allow configured admins: admin_user_ban.js
    
    * Allow configured admins: admin_user_ban_view.js
    
    * Allow configured admins: admin_user_search.js
    
    * Allow configured admins: admin_user_search_start.js
    
    * Allow configured admins: admin_user_unban.js
    
    * Allow configured admins: admin_user_view.js
    
    * Allow configured admins: admin_users.js
    
    * Allow configured admins: admin_withdraw.js
    
    * Allow configured admins: admin_withdraw_approve.js
    
    * Allow configured admins: admin_withdraw_reject.js
    
    * Allow configured admins: admin_withdraw_view.js
    
    * Allow configured admins: admin_withdrawals.js
    
    * Allow configured admins: currency_edit.js
    
    * Allow configured admins: currency_save.js
    
    * Allow configured admins: daily_bonus_mode.js
    
    * Allow configured admins: daily_bonus_reward_save.js
    
    * Allow configured admins: daily_bonus_reward_start.js
    
    * Allow configured admins: daily_bonus_toggle.js
    
    * Allow configured admins: referral_reward_save.js
    
    * Allow configured admins: referral_reward_start.js
    
    * Allow configured admins: referral_toggle.js
    
    * Allow configured admins: task_create.js
    
    * Allow configured admins: task_description.js
    
    * Allow configured admins: task_description_skip.js
    
    * Allow configured admins: task_description_start.js
    
    * Allow configured admins: task_edit_field.js
    
    * Allow configured admins: task_edit_save.js
    
    * Allow configured admins: task_limit_custom.js
    
    * Allow configured admins: task_limit_save.js
    
    * Allow configured admins: task_limit_start.js
    
    * Allow configured admins: task_limit_unlimited.js
    
    * Allow configured admins: task_preview.js
    
    * Allow configured admins: task_publish.js
    
    * Allow configured admins: task_reward.js
    
    * Allow configured admins: task_reward_start.js
    
    * Allow configured admins: task_target.js
    
    * Allow configured admins: task_target_start.js
    
    * Allow configured admins: task_title.js
    
    * Allow configured admins: task_title_start.js
    
    * Allow configured admins: task_type.js
    
    * Allow configured admins: task_verification.js
    
    * Allow configured admins: withdraw_channel_save.js
    
    * Allow configured admins: withdraw_channel_start.js
    
    * Allow configured admins: withdraw_method_edit.js
    
    * Allow configured admins: withdraw_method_save.js
    
    * Allow configured admins: withdraw_method_toggle.js
    
    * Allow configured admins: fj_add.js
    
    * Remove unavailable admin panel actions

--- commands/ADMIN/admin_broadcast_send.js
+++ commands/ADMIN/admin_broadcast_send.js
@@ -31,9 +31,17 @@ CMD*/
 var ownerId =
   Bot.getProperty("owner_id");
 
+var hasAdminAccess = (user.telegramid == ownerId);
+if (!hasAdminAccess) {
+  var admins = Bot.getProperty("bot_admins") || [];
+  for (var ai = 0; ai < admins.length; ai++) {
+    if (admins[ai].user_id == user.telegramid) { hasAdminAccess = true; break; }
+  }
+}
+
 if (
   !ownerId ||
-  user.telegramid != ownerId
+  !hasAdminAccess
 ) {
 
   Bot.sendMessage(
--- commands/ADMIN/fj_add.js
+++ commands/ADMIN/fj_add.js
@@ -27,6 +27,14 @@ CMD*/
 
 var ownerId = Bot.getProperty("owner_id");
 
+var hasAdminAccess = (user.telegramid == ownerId);
+if (!hasAdminAccess) {
+  var admins = Bot.getProperty("bot_admins") || [];
+  for (var ai = 0; ai < admins.length; ai++) {
+    if (admins[ai].user_id == user.telegramid) { hasAdminAccess = true; break; }
+  }
+}
+
 /*
   TEMPORARY FIRST-OWNER INITIALIZATION
 
@@ -49,7 +57,7 @@ if (!ownerId) {
 
 /* ---------- OWNER CHECK ---------- */
 
-if (user.telegramid != ownerId) {
+if (!hasAdminAccess) {
   Bot.sendMessage("⛔ You don't have permission to use this command.");
   return;
 }
